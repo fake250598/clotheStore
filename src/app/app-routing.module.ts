e8e9506a1242134edb8393c392cc6392ce89b928
Joel Infante <joel250598@gmail.com>
2020-01-13 11:53:12 -0500

    creating register view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ContactUsComponent } from './components/contact-us/contact-us.component
 import { CategoryComponent } from './components/category/category.component';
 import { CategoryviewComponent } from './components/category/categoryview/categoryview.component';
 import { ItemDetailComponent } from './components/item-detail/item-detail.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
@@ -14,6 +15,7 @@ const routes: Routes = [
     {path: 'view', component: CategoryviewComponent, pathMatch: 'full'},
   ]},
   {path: 'item-detail', component: ItemDetailComponent},
+  {path: 'register', component: RegisterComponent},
 ]
 
 @NgModule({
---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    const signUpButton = document.getElementById('signUp');
+    const signInButton = document.getElementById('signIn');
+    const container = document.getElementById('container');
+
+    signUpButton.addEventListener('click', () => {
+      container.classList.add("right-panel-active");
+    });
+
+    signInButton.addEventListener('click', () => {
+      container.classList.remove("right-panel-active");
+    });
+    
+  }
+
+}
